63c3e356a6caad9fd37c52b25bf169cace00ac0c
tobias <tobias.omondi@student.moringaschool.com>
2023-09-04 19:49:38 +0300

    assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,20 @@
 import React from "react";
-import ProjectItem from "./ProjectItem";
+import ProjectItem from "./ProjectItem"; // Assuming you have a ProjectItem component
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id} // Assuming each project object has a unique id
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
